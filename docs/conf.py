f7c27d88d5a968a8f36bf55f0e56ac9029390766
peterjunpark <peter.park@amd.com>
2026-09-10 13:27:07 -0400

    chore: clean up custom Sphinx extensions and stale files (#36)
    * use selector extension from rocm-docs-core >= 1.40
    
    * update conf.py
    
    * remove unused `exclude/` files

--- docs/conf.py
+++ docs/conf.py
@@ -115,7 +115,7 @@ external_toc_path = "./sphinx/_toc.yml"
 sys.path.append(str(DOCS_DIR / "extension"))
 extensions = [
     "rocm_docs",
-    "rocm_docs_custom.selector",
+    "rocm_docs.selector",
     "rocm_docs_custom.matrix",
     "rocm_docs_custom.icon",
     "rocm_docs_custom.csv_list_to_table",
@@ -201,7 +201,6 @@ rst_prolog = "\n".join(
 numfig = False
 
 exclude_patterns = [
-    "exclude/**",
     "**/include/**",
     "**/extension/**",
     "**/images/**",
